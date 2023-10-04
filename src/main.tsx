ede49029d722f8637d6580ff388b58656e52152b
noname2048 <noname2048@users.noreply.github.com>
2023-10-04 22:19:24 +0900

    [#1] Setup process
    * chore: update docs
    
    * chore: do pnpm install
    
    * chore: install react-router-dom
    
    * feat: create blank app
    
    * chore: setup tsconfig @ alias
    
    * feat: register login page
    
    * chore: install tailwind
    
    * feat: setup tailwind
    
    * chore: install clsx tailwind-merge
    
    * feat: add libs for className

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './global.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider  router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const LoginPage: React.FC = () => {
+    return <div>login</div>
+}
+
+export default LoginPage;
