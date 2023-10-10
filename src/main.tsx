a4ce900d00f0446467301b0d7e9276302882d39b
noname2048 <sungwook.csw@gmail.com>
2023-10-10 21:52:27 +0900

    feat: apply Mypage

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 
-import Authorization from "@/domains/login/components/Authorization.tsx";
+import App from "@/App.tsx";
 import { worker } from "@/mocks/browser";
-import RouterInfo from "@/router/RouterInfo.tsx";
 
 import "./global.css";
 
 if (process.env.NODE_ENV === "development") {
-  worker.start();
+  worker.start().then(() => {});
 }
-
-const router = createBrowserRouter(
-  RouterInfo.map((routerInfo) => {
-    return routerInfo.type === "private"
-      ? {
-          path: routerInfo.path,
-          element: <Authorization>{routerInfo.element}</Authorization>,
-        }
-      : {
-          path: routerInfo.path,
-          element: routerInfo.element,
-        };
-  }),
-);
-
-console.log(router);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <RouterProvider router={router} />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>,
 );
